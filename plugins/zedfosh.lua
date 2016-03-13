72bb48e2709ebe87fcc32007e8852f4621f20bed
padrinoo2 <padrinoo200@gmail.com>
2016-03-13 15:08:39 +0330

    Zed fosh

---
+++ plugins/zedfosh.lua
@@ -0,0 +1,16 @@
+do
+
+function run(msg, matches)
+  return 'جهت ضد فوش کردن گروه خود ابتدا ربات زیر رو اد کنید سپس دستور #zedfosh را اعمال کنید :\n@vagirbot' 
+  --[[ Checkout https://github.com/padrinoo2/1vagir1
+  --GNU GPL v2 license.]]
+end
+
+return {
+  patterns = {
+    "^#zedfosh$"
+  }, 
+  run = run 
+}
+
+end
